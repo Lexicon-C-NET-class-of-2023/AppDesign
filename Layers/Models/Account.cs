dac70986e9da9f10976814573327ec5ec0b6ad22
Constantin Pantesi <constantin.pantesi@yahoo.com>
2023-10-16 17:09:48 +0200

    Created temporary JSON file of rentals with a couple of objects, added new properties to class Account

--- Layers/Models/Account.cs
+++ Layers/Models/Account.cs
@@ -9,8 +9,14 @@ namespace Layers.Models
     public class Account
     {
         public int Id { get; set; }
-        public int Age { get; set; }
+        public DateTime Registered { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public int age { get; set; }
+        public string? Address { get; set; }
+
+        public int Phonenumber { get; set; }
         public decimal Balance { get; set; }
-        public string Name { get; set; }
+        public string? Email { get; set; }
     }
 }
