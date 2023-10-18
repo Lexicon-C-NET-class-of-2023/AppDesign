f0e8baacb6d54cf89686904df55c829205993787
fateh bouretoua <bouretoua_fateh@hotmail.com>
2023-10-19 00:23:23 +0200

    Create Rental Repository class

---
+++ Layers/Services/RentalServices.cs
@@ -0,0 +1,24 @@
+﻿namespace Layers.Models
+{
+    public class RentalServices
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public bool Available { get; set; }
+        public int PricePerDay { get; set; }
+        public int PricePerWeek { get; set; }
+        public DateTime DateOfRent { get; set; }
+        public DateTime DateOfReturn { get; set; }
+
+        public RentalServices(bool available, DateTime dateOfRent, DateTime dateOfReturn)
+        {
+            DateOfRent = dateOfRent;
+            DateOfReturn = dateOfReturn;
+
+            Model = "Husqvarna LB251S";
+            Available = available || true;
+            PricePerDay = 100;
+            PricePerWeek = 500;
+        }
+    }
+}
