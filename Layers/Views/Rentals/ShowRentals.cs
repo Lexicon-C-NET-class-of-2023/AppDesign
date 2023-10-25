18d8bfd9b05df2c85a11552afc0e856a6462fa52
ArnT81 <anders_soderberg@hotmail.com>
2023-10-25 18:39:10 +0200

    fixed "DB" since a removed accounts from file

--- Layers/Views/Rentals/ShowRentals.cs
+++ Layers/Views/Rentals/ShowRentals.cs
@@ -7,12 +7,13 @@ namespace Layers.Views.Rentals
     {
         public static void ShowAllRentals(List<Rental> rentals)
         {
-            Console.WriteLine("\n\n\nRegistered Hires\n");
+            Console.WriteLine(rentals.Count() > 0 ? "\n\n\nRegistered Hires\n" : "\n\n\nThere are no Registered Hires\n");
             foreach (var rental in rentals)
             {
                 Console.WriteLine("Id: " + rental.Id);
                 Console.WriteLine("RentedByAccountId: " + rental.RentedByAccountId);
                 Console.WriteLine("lownMoverId: " + rental.LownMoverId);
+                Console.WriteLine("unit: " + rental.HowLong);
                 Console.WriteLine("Period: " + rental.Period);
                 Console.WriteLine("Rented: " + rental.FromDate);
                 Console.WriteLine("Returns: " + rental.ToDate);
